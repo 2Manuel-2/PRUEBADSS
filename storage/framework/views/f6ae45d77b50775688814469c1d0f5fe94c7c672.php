b47a0594427897cd7d6d756edbada61d59d504d7
2Manuel-2 <99776318+2Manuel-2@users.noreply.github.com>
2023-04-21 18:33:01 -0600

    TWO

---
+++ storage/framework/views/f6ae45d77b50775688814469c1d0f5fe94c7c672.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit de editoriales</title>
+</head>
+<body>
+    <h3>Modificando editorial con ID <?php echo e($id); ?> y <?php echo e($nombre); ?></h3>
+</body>
+</html><?php /**PATH C:\wamp64\www\demo-laravel-DSS\resources\views/editoriales/edit.blade.php ENDPATH**/ ?>
